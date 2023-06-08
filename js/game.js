d6a5242b303ce8f24cafc445374ec020063535c7
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-06-08 17:27:07 +0000

    game js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,18 @@
-/* global Phaser
+/* global Phaser*/
 // Copyright (c) 2023 Olivia B-R All rights reserved //
 // Created by: Olivia B-R
 // Created on: April 2023
-// This is the Phaser 3 game configuration file
+// This is the game js file
+
+
+//scene import statements 
+import SpashScene from './splashScene.js'
+import titleScene from './titleScene.js'
+
+// create the new scenes
+const splashScene = new SpashScene()
+const titleScene = new TitleScene()
+
 /**
 * Start Phaser Game.
 */
@@ -17,13 +27,21 @@ const config = {
         },
     },
 
-    
+    // set background color
     backgroundColor: 0x5f6e7a,
     scale: {
         mode: Phaser.Scale.FIT,
+        // we place it in the middle of the page.
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     }
 
     const game = new Phaser.Game (config)
     console.log(game)
+
+    // load scenes
+    game.scene.add("splashScene", splashScene)
+    game.scene.add("titleScene", titleScene)
+
+    // the start scene
+    game.scene.start("splashScene")
---
+++ js/splashScene.js
@@ -0,0 +1,23 @@
+/* global Phaser*/
+// Copyright (c) 2023 Olivia B-R All rights reserved //
+// Created by: Olivia B-R
+// Created on: April 2023
+// This is the splashscene js file
+
+/**
+ * this class is the splashscene
+ */
+
+class SplashScene extends Phaser.Scene {
+    constructor () {
+        super({ key: 'splashScene' })
+    }
+
+    /**
+     * can be defined on your own scenes
+     * use it create your game objects
+     * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start()
+     */
+    create (data) {
+        //pass
+    }
